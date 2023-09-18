227967ec66001a9b7cdd19dbc82bb3929310156b
Hazelnut76 <131495891+Hazelnut76@users.noreply.github.com>
2023-09-18 15:18:36 +0200

    Imported project code to react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="weather-app-wrap">
+          <div className="weather-app">
+            <form id="search-form" className="mb-4">
+              <div className="row">
+                <div className="col-6">
+                  <input
+                    type="search"
+                    placeholder="Type a city..."
+                    className="form-control"
+                    id="city-input"
+                    autocomplete="off"
+                  />
+                </div>
+                <div className="col-3">
+                  <input
+                    type="submit"
+                    value="Search"
+                    className="btn btn-primary w-100"
+                  />
+                </div>
+                <div className="col-3">
+                  <input
+                    type="submit"
+                    value="Current"
+                    className="btn btn-success w-100"
+                  />
+                </div>
+              </div>
+            </form>
+            <div className="overview">
+              <h1 id="city">Harare</h1>
+              <ul>
+                <li>
+                  Updated on <span id="date">Mon, Sep, 18, 2023 14:51</span>
+                </li>
+                <li id="description">Clear</li>
+              </ul>
+            </div>
+            <div className="row">
+              <div className="col-6">
+                <div className="d-flex weather-temperature">
+                  <img src="" alt="Clear" id="icon" />
+                  <div>
+                    <strong id="temperature">25</strong>
+                    <span className="units">°C</span>
+                  </div>
+                </div>
+              </div>
+              <div className="col-6">
+                <ul>
+                  <li>
+                    Humidity: <span id="humidity">39</span>%
+                  </li>
+                  <li>
+                    Wind: <span id="wind">4</span>km/h
+                  </li>
+                  <li>
+                    Feels like: <span id="feeling">27</span>°C
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="weather-forecast" id="forecast"></div>
+          </div>
+          <small>
+            <a
+              href="https://github.com/Hazelnut76/vanilla-weather-app"
+              target="_blank"
+            >
+              Open-source code,
+            </a>
+            by Emmah Nyashanu
+          </small>
+        </div>
+      </div>
+      <script src="src/index.js"></script>
     </div>
   );
 }
-
-export default App;
---
+++ src/Weather.js
@@ -0,0 +1,135 @@
+
+export default function Weather() {
+  return function formatDate(timestamp) {
+  let date = new Date(timestamp);
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let month = months[date.getMonth()];
+
+  let dates = date.getDate();
+
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${month}, ${dates} ${hours}:${minutes}`;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+    <div class="col-2">
+      <div class="weather-forecast-date">${formatDay(forecastDay.time)}</div>
+      <img
+        src="${forecastDay.condition.icon_url}"
+        alt=""
+        width="42"
+      />
+      <div class="weather-forecast-temperature">
+        <span class="weather-forecast-temperature-max"> ${Math.round(
+          forecastDay.temperature.maximum
+        )}° </span>
+        <span class="weather-forecast-temperature-min"> ${Math.round(
+          forecastDay.temperature.minimum
+        )}° </span>
+      </div>
+    </div>`;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "6f41b6t84b9ef1ba6odf3722ea3400aa";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${coordinates.latitude}&lon=${coordinates.longitude}&key=${apiKey}&units=metric`;
+  
+}
+
+function displayTemperture(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+  let feelingElement = document.querySelector("#feeling");
+
+  
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+ 
+  iconElement.setAttribute("src", response.data.condition.icon_url);
+  iconElement.setAttribute("alt", response.data.condition.description);
+  feelingElement.innerHTML = Math.round(response.data.temperature.feels_like);
+
+  getForecast(response.data.coordinates);
+}
+
+function search(city) {
+  let apiKey = "6f41b6t84b9ef1ba6odf3722ea3400aa";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+}
